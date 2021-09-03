2b397c84163e72618215187ac033f7641e560eef
BluABK <bluabk@bluabk.net>
2021-09-03 15:15:37 +0200

    Create classes Rook and Bishop based on the new abstract Piece class.

---
+++ SimpleChess/Bishop.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleChess
+{
+    internal class Bishop : Piece
+    {
+        private const string symbol = "LPR";
+        public Bishop() 
+            : base(symbol)
+        {
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ SimpleChess/Rook.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleChess
+{
+    internal class Rook : Piece
+    {
+        private const string symbol = "TRN";
+        public Rook()
+            : base(symbol)
+        {
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
